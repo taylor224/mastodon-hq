70e2cb671187821e28b306bb175d5a061ac25532
Taylor Starfield <taylor224@users.noreply.github.com>
2022-11-30 18:51:55 +0900

    Update media_attachment.rb

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -38,10 +38,10 @@ class MediaAttachment < ApplicationRecord
 
   MAX_DESCRIPTION_LENGTH = 1_500
 
-  IMAGE_LIMIT = 10.megabytes
-  VIDEO_LIMIT = 40.megabytes
+  IMAGE_LIMIT = 30.megabytes
+  VIDEO_LIMIT = 200.megabytes
 
-  MAX_VIDEO_MATRIX_LIMIT = 2_304_000 # 1920x1200px
+  MAX_VIDEO_MATRIX_LIMIT = 8_847_360 # 4096x2160px
   MAX_VIDEO_FRAME_RATE   = 60
 
   IMAGE_FILE_EXTENSIONS = %w(.jpg .jpeg .png .gif .webp .heic .heif .avif).freeze
@@ -68,12 +68,12 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 2_073_600, # 1920x1080px
+      pixels: 36_000_000, # 6000*6000px
       file_geometry_parser: FastGeometryParser,
     }.freeze,
 
     small: {
-      pixels: 230_400, # 640x360px
+      pixels: 786_432, # 1024x768px
       file_geometry_parser: FastGeometryParser,
       blurhash: BLURHASH_OPTIONS,
     }.freeze,
@@ -169,7 +169,7 @@ class MediaAttachment < ApplicationRecord
   }.freeze
 
   GLOBAL_CONVERT_OPTIONS = {
-    all: '-quality 90 +profile "!icc,*" +set modify-date +set create-date',
+    all: '-quality 94 +profile "!icc,*" +set modify-date +set create-date',
   }.freeze
 
   belongs_to :account,          inverse_of: :media_attachments, optional: true
